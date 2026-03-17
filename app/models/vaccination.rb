6120db86e59256b65d19c4a19615a1fb1055bb0d
Sarah Abila <abila.sarah83@gmail.com>
2026-03-17 15:32:23 +0100

    vaccination

--- app/models/vaccination.rb
+++ app/models/vaccination.rb
@@ -1,19 +1,27 @@
 class Vaccination < ApplicationRecord
   #  l'ID s'appelle baby_id mais cherche dans UserBaby
   belongs_to :user_baby, class_name: "UserBaby", foreign_key: "user_baby_id"
+ VACCINES_LIST = [
+    { name: "BCG (tuberculose)",        age: 0   },
+    { name: "Hépatite B - dose 1",      age: 0   },
+    { name: "DTCaP-Hib-HB - dose 1",   age: 2   },
+    { name: "Pneumocoque - dose 1",     age: 2   },
+    { name: "Méningocoque B - dose 1",  age: 2   },
+    { name: "Rotavirus - dose 1",       age: 2   },
+    { name: "DTCaP-Hib-HB - dose 2",   age: 4   },
+    { name: "Pneumocoque - dose 2",     age: 4   },
+    { name: "Méningocoque B - dose 2",  age: 4   },
+    { name: "Rotavirus - dose 2",       age: 4   },
+    { name: "Méningocoque C - dose 1",  age: 5   },
+    { name: "DTCaP-Hib-HB - dose 3",   age: 11  },
+    { name: "Pneumocoque - dose 3",     age: 11  },
+    { name: "ROR - dose 1",             age: 12  },
+    { name: "Méningocoque C - dose 2",  age: 12  },
+    { name: "ROR - dose 2",             age: 16  },
+    { name: "DTCaP - rappel 6 ans",     age: 72  },
+    { name: "DTCaP - rappel ado",       age: 132 }
+  ].freeze
 
-  #def due_date
-  # Sécurité : si l'âge est nil, on considère 0 mois
- # months_to_add = age || 0
-
-  # On s'assure que user_baby et sa birth_date existent
-  #if user_baby&.birth_date
-   # user_baby.birth_date + months_to_add.months
- #else
-   # Date.today
-  #end
-#end
-#
  def date_for_age(age_in_months)
     birth_date + age_in_months.months
   end
